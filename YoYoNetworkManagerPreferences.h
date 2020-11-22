d363188ee9ea9338026a7784f347ddd2eeb26d7b
David Chatting <david@davidchatting.com>
2020-11-22 16:26:01 +0000

    + YoYoNetworkManagerPreferences

---
+++ YoYoNetworkManagerPreferences.h
@@ -0,0 +1,15 @@
+#ifndef YoYoNetworkManagerPreferences_h
+#define YoYoNetworkManagerPreferences_h
+
+#include "YoYoNetworkManager.h"
+
+#include <Preferences.h>
+
+class YoYoNetworkManagerPreferences
+{
+    private:
+        Preferences preferences;
+    public:
+};
+
+#endif
