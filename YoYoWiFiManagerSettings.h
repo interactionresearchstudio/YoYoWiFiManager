273b41aa34a995a50446ff8310028ec6a48dade0
David Chatting <david@davidchatting.com>
2020-12-09 16:45:08 +0000

    Update YoYoWiFiManagerSettings.h

--- YoYoWiFiManagerSettings.h
+++ YoYoWiFiManagerSettings.h
@@ -4,7 +4,7 @@
 class YoYoWiFiManagerSettings {
   public:
     virtual int getNumberOfNetworkCredentials() = 0;
-    virtual void addNetwork(const char *ssid, const char *password) = 0;
+    virtual void addNetwork(const char *ssid, const char *password, bool autosave = true) = 0;
     virtual void getSSID(int n, char *ssid) = 0;
     virtual void getPassword(int n, char *password) = 0;
 
