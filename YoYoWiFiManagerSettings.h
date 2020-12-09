dc0b324cddb9d46a03302851e8b87555b109deba
David Chatting <david@davidchatting.com>
2020-12-09 12:19:31 +0000

    hasNetworkCredentials() can be defined here

--- YoYoWiFiManagerSettings.h
+++ YoYoWiFiManagerSettings.h
@@ -3,11 +3,14 @@
 
 class YoYoWiFiManagerSettings {
   public:
-    virtual bool hasNetworkCredentials() = 0;
     virtual int getNumberOfNetworkCredentials() = 0;
     virtual void addNetwork(const char *ssid, const char *password) = 0;
     virtual void getSSID(int n, char *ssid) = 0;
     virtual void getPassword(int n, char *password) = 0;
+
+    bool hasNetworkCredentials() {
+      return(getNumberOfNetworkCredentials() > 0);
+    }
 };
 
 #endif
