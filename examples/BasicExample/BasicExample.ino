0b4c2c23f3912df743b45daa2b1164c18fd91674
David Chatting <david@davidchatting.com>
2020-12-18 16:00:20 +0000

    tidyied-up status handling

--- examples/BasicExample/BasicExample.ino
+++ examples/BasicExample/BasicExample.ino
@@ -4,8 +4,6 @@
 YoYoWiFiManager wifiManager;
 Settings *settings;
 
-uint8_t currentStatus = YoYoWiFiManager::YY_IDLE_STATUS;
-
 void setup() {
   Serial.begin(115200);
 
@@ -15,28 +13,21 @@ void setup() {
   wifiManager.begin("YoYoMachines", "blinkblink", true);
 }
 
-/*
-      YY_NO_SHIELD        = WL_NO_SHIELD,
-      YY_IDLE_STATUS      = WL_IDLE_STATUS,
-      YY_NO_SSID_AVAIL    = WL_NO_SSID_AVAIL,
-      YY_SCAN_COMPLETED   = WL_SCAN_COMPLETED,
-      YY_CONNECTED        = WL_CONNECTED,
-      YY_CONNECT_FAILED   = WL_CONNECT_FAILED,
-      YY_CONNECTION_LOST  = WL_CONNECTION_LOST,
-      YY_DISCONNECTED     = WL_DISCONNECTED,
-      YY_CONNECTED_PEER_CLIENT,
-      YY_CONNECTED_PEER_SERVER
-*/
-
 void loop() {
-  uint8_t s = wifiManager.update();
-  if(s != currentStatus) {
-    currentStatus = s;
-    printStatus();
-  }
+  uint8_t currentStatus = wifiManager.update();
 
-  if(currentStatus == WL_CONNECTED) {
-
+  switch(currentStatus) {
+    case YY_CONNECTED:
+      //Same as WL_CONNECTED - when there is an Internet connection
+      break;
+    case YY_CONNECTED_PEER_CLIENT:
+      break;
+    case YY_CONNECTED_PEER_SERVER:
+      break;
+    case YY_DISCONNECTED:
+      break;
+    default:
+      break;
   }
 }
 
@@ -67,38 +58,6 @@ bool onYoYoCommandPOST(const String &url, JsonVariant json) {
   }
 
   return(success);
-}
-
-void printStatus() {
-  switch (currentStatus) {
-    case YoYoWiFiManager::YY_CONNECTED:
-      Serial.println("YY_CONNECTED");
-      break;
-    case YoYoWiFiManager::YY_IDLE_STATUS:
-      Serial.println("YY_IDLE_STATUS");
-      break;
-    case YoYoWiFiManager::YY_NO_SSID_AVAIL:
-      Serial.println("YY_NO_SSID_AVAIL");
-      break;
-    case YoYoWiFiManager::YY_SCAN_COMPLETED:
-      Serial.println("YY_SCAN_COMPLETED");
-      break;
-    case YoYoWiFiManager::YY_CONNECT_FAILED:
-      Serial.println("YY_CONNECT_FAILED");
-      break;
-    case YoYoWiFiManager::YY_CONNECTION_LOST:
-      Serial.println("YY_CONNECTION_LOST");
-      break;
-    case YoYoWiFiManager::YY_DISCONNECTED:
-      Serial.println("YY_DISCONNECTED");
-      break;
-    case YoYoWiFiManager::YY_CONNECTED_PEER_CLIENT:
-      Serial.println("YY_CONNECTED_PEER_CLIENT");
-      break;
-    case YoYoWiFiManager::YY_CONNECTED_PEER_SERVER:
-      Serial.println("YY_CONNECTED_PEER_SERVER");
-      break;
-  }   
 }
 
 String generateID() {
