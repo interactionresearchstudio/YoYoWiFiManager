7dfcd35985a055354cf7124f90f8112a5205f2d7
David Chatting <david@davidchatting.com>
2020-12-27 18:23:41 +0000

    Simplify example

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -8,68 +8,15 @@ void setup() {
   Serial.begin(115200);
 
   settings = new Settings(512); //Settings must be created here in Setup() as contains call to EEPROM.begin() which will otherwise fail
-  
-  wifiManager.init(settings, onYoYoCommandGET, onYoYoCommandPOST, true);
-  wifiManager.begin("YoYoMachines", "blinkblink", true);
+  wifiManager.init(settings);
+
+  wifiManager.begin("YoYoMachines", "blinkblink");
 }
 
 void loop() {
-  uint8_t currentStatus = wifiManager.update();
+  uint8_t wifiStatus = wifiManager.update();
 
-  switch(currentStatus) {
-    case YY_CONNECTED:
-      //Same as WL_CONNECTED - when there is an Internet connection
-      break;
-    case YY_CONNECTED_PEER_CLIENT:
-      break;
-    case YY_CONNECTED_PEER_SERVER:
-      break;
-    case YY_DISCONNECTED:
-      break;
-    default:
-      break;
+  if(wifiStatus == YY_CONNECTED) {
+    //YY_CONNECTED is equal to WL_CONNECTED
   }
-}
-
-bool onYoYoCommandGET(const String &url, JsonVariant json) {
-  bool success = false;
-
-  Serial.println("onYoYoCommandGET " + url);
-  
-  if(url.equals("/yoyo/settings") && settings) {
-    success = true;
-    json.set(*settings);
-  }
-
-  return(success);
-}
-
-bool onYoYoCommandPOST(const String &url, JsonVariant json) {
-  bool success = false;
-  
-  Serial.println("onYoYoCommandPOST " + url);
-  serializeJson(json, Serial);
-
-  if(url.equals("/yoyo/settings")) {
-    //an alternative to using the built-in /yoyo/credentials...
-    success = wifiManager.setCredentials(json);
-    if(success) wifiManager.connect();
-
-    //TODO: set any other values from the payload
-  }
-
-  return(success);
-}
-
-String generateID() {
-  uint32_t id = 0;
-
-  //the id is the lowest 4 bytes of the MAC adddress:
-  #if defined(ESP8266)
-    id = ESP.getChipId();
-  #elif defined(ESP32)
-    id = ESP.getEfuseMac() && 0xFFFFFFFF;
-  #endif
-
-  return(String(id));
 }
