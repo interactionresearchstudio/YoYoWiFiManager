32722546db759a987f318a77d3764145d0c0919a
David Chatting <david@davidchatting.com>
2021-01-06 13:06:01 +0000

    Tidy-up

--- examples/BasicWithEndpoints/BasicWithEndpoints.ino
+++ examples/BasicWithEndpoints/BasicWithEndpoints.ino
@@ -1,5 +1,5 @@
-#include "YoYoWiFiManager.h"
-#include "YoYoSettings.h"
+#include <YoYoWiFiManager.h>
+#include <YoYoSettings.h>
 
 YoYoWiFiManager wifiManager;
 YoYoSettings *settings;
@@ -8,25 +8,38 @@ void setup() {
   Serial.begin(115200);
 
   settings = new YoYoSettings(512); //Settings must be created here in Setup() as contains call to EEPROM.begin() which will otherwise fail
-  
-  wifiManager.init(settings, onYoYoCommandGET, onYoYoCommandPOST, true);
-  wifiManager.begin("YoYoMachines", "blinkblink", true);
+  wifiManager.init(settings, onceConnected, onYoYoCommandGET, onYoYoCommandPOST, true);
+
+  wifiManager.begin("YoYoMachines", "blinkblink");
+}
+
+void onceConnected() {
+  //When status changes to YY_CONNECTED - loop() must be being called
 }
 
 void loop() {
-  uint8_t currentStatus = wifiManager.loop();
+  uint8_t wifiStatus = wifiManager.loop();
 
-  switch(currentStatus) {
+  switch(wifiStatus) {
+    case YY_NO_SHIELD:
+      break;
+    case YY_IDLE_STATUS:
+      break;
+    case YY_NO_SSID_AVAIL:
+      break;
+    case YY_SCAN_COMPLETED:
+      break;
     case YY_CONNECTED:
-      //Same as WL_CONNECTED - when there is an Internet connection
+      break;
+    case YY_CONNECT_FAILED:
+      break;
+    case YY_CONNECTION_LOST:
+      break;
+    case YY_DISCONNECTED:
       break;
     case YY_CONNECTED_PEER_CLIENT:
       break;
     case YY_CONNECTED_PEER_SERVER:
-      break;
-    case YY_DISCONNECTED:
-      break;
-    default:
       break;
   }
 }
@@ -59,17 +72,4 @@ bool onYoYoCommandPOST(const String &url, JsonVariant json) {
   }
 
   return(success);
-}
-
-String generateID() {
-  uint32_t id = 0;
-
-  //the id is the lowest 4 bytes of the MAC adddress:
-  #if defined(ESP8266)
-    id = ESP.getChipId();
-  #elif defined(ESP32)
-    id = ESP.getEfuseMac() && 0xFFFFFFFF;
-  #endif
-
-  return(String(id));
 }
