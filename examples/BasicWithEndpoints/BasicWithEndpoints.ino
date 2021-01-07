bf52e9415589d6785d619d9b4cc8e18a4c6344d9
David Chatting <david@davidchatting.com>
2021-01-07 12:11:59 +0000

    being explicit about autoconnect parameter

--- examples/BasicWithEndpoints/BasicWithEndpoints.ino
+++ examples/BasicWithEndpoints/BasicWithEndpoints.ino
@@ -10,7 +10,7 @@ void setup() {
   settings = new YoYoSettings(512); //Settings must be created here in Setup() as contains call to EEPROM.begin() which will otherwise fail
   wifiManager.init(settings, onceConnected, onYoYoCommandGET, onYoYoCommandPOST, true);
 
-  wifiManager.begin("YoYoMachines", "blinkblink");
+  wifiManager.begin("YoYoMachines", "blinkblink", true);
 }
 
 void onceConnected() {
