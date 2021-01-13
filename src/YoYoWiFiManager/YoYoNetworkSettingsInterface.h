e6934b7891af21e90546520a3d0f909939f84c5d
David Chatting <david@davidchatting.com>
2021-01-13 21:19:49 +0000

    +getNetwork() and now passwords updated if network already exists in the list

--- src/YoYoWiFiManager/YoYoNetworkSettingsInterface.h
+++ src/YoYoWiFiManager/YoYoNetworkSettingsInterface.h
@@ -4,9 +4,10 @@
 class YoYoNetworkSettingsInterface {
   public:
     virtual int getNumberOfNetworkCredentials() = 0;
-    virtual void addNetwork(const char *ssid, const char *password, bool autosave = true) = 0;
+    virtual bool addNetwork(const char *ssid, const char *password, bool autosave = true) = 0;
     virtual void getSSID(int n, char *ssid) = 0;
     virtual void getPassword(int n, char *password) = 0;
+    virtual int getNetwork(const char *ssid) = 0;
 
     bool hasNetworkCredentials() {
       return(getNumberOfNetworkCredentials() > 0);
