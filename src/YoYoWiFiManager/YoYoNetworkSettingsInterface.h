ac8b6c461240ca5eb0f4b5541f852792358565e6
David Chatting <david@davidchatting.com>
2021-06-16 18:06:28 +0100

    getSSID() + getPassword() catch error - now returns success

--- src/YoYoWiFiManager/YoYoNetworkSettingsInterface.h
+++ src/YoYoWiFiManager/YoYoNetworkSettingsInterface.h
@@ -5,8 +5,8 @@ class YoYoNetworkSettingsInterface {
   public:
     virtual int getNumberOfNetworkCredentials() = 0;
     virtual bool addNetwork(const char *ssid, const char *password, bool autosave = true) = 0;
-    virtual void getSSID(int n, char *ssid) = 0;
-    virtual void getPassword(int n, char *password) = 0;
+    virtual bool getSSID(int n, char *ssid) = 0;
+    virtual bool getPassword(int n, char *password) = 0;
     virtual int getNetwork(const char *ssid) = 0;
 
     virtual bool removeNetwork(const char *ssid, bool autosave = true) = 0;
