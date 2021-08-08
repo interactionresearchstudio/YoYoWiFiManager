4fd38604ccfb9830947300a0d85ea83bf3ec6553
David Chatting <david@davidchatting.com>
2021-08-08 20:09:58 +0100

    Added comment about how begin() is used

--- examples/PeerNetwork/PeerNetwork.ino
+++ examples/PeerNetwork/PeerNetwork.ino
@@ -9,6 +9,10 @@ void setup() {
   Serial.begin(115200);
 
   wifiManager.init();
+
+  //Attempt to connect to a WiFi network previously saved in the settings, 
+  //if one can not be found start a captive portal called "YoYoMachines", 
+  //with a password of "blinkblink" to configure a new one:
   wifiManager.begin("YoYoMachines", "blinkblink", false);
 }
 
--- examples/Vue/Vue.ino
+++ examples/Vue/Vue.ino
@@ -40,6 +40,10 @@ void setup() {
 
   settings = new YoYoSettings(512); //Settings must be created here in Setup() as contains call to EEPROM.begin() which will otherwise fail
   wifiManager.init(settings, NULL, onYoYoMessageGET, onYoYoMessagePOST, true);
+
+  //Attempt to connect to a WiFi network previously saved in the settings, 
+  //if one can not be found start a captive portal called "YoYoMachines", 
+  //with a password of "blinkblink" to configure a new one:
   wifiManager.begin("YoYoMachines", "blinkblink", false);
   
   pinMode(LED_RED_PIN, OUTPUT);
